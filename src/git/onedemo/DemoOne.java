4555080601cebca9c436a2b287e5b95da465bf7e
winta <winta@DESKTOP-G9LOJV9>
2021-11-24 23:38:46 -0500

    new commit

--- src/git/onedemo/DemoOne.java
+++ src/git/onedemo/DemoOne.java
@@ -3,7 +3,7 @@ package git.onedemo;
 public class DemoOne {
 
 	public static void main(String[] args) {
- System.out.println(" First git push demo");
+		System.out.println(" First git push demo");
 	}
 
 }
